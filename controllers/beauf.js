0076d715ce3dab77df2c9e769b35c8bda5dade82
Emmanuel <fathoms.contact@gmail.com>
2023-02-16 17:41:33 +0100

    upvotes & downvotes

--- controllers/beauf.js
+++ controllers/beauf.js
@@ -1,12 +1,17 @@
 const Beauf = require('../models/beauf');
 
 exports.createBeauf = (req, res, next) => {
+    const bolArray = [true, false];
     const beauf = new Beauf({
-        ...req.body
+        ...req.body,
+        upVotes: 0,
+        downVotes: 0,
+        isBeauf: bolArray[Math.floor(Math.random() * bolArray.length)],
+        originalResult: req.body.isBeauf
     });
     beauf.save()
         .then(() => {
-            res.status(201).json({ message: 'Beauf enregistré !', id: beauf._id }) 
+            res.status(201).json({ message: 'Beauf enregistré !', id: beauf._id })
         })
         .catch(error => res.status(400).json({ error }));
 };
@@ -22,3 +27,27 @@ exports.getAllBeauf = (req, res, next) => {
         .then(beaufs => res.status(200).json(beaufs))
         .catch(error => res.status(400).json({ error }));
 };
+
+exports.vote = (req, res, next) => {
+    Beauf.findOne({ id: req.params.id })
+        .then(beauf => {
+            if (req.body.upVote) {
+                beauf.upVotes++;
+            } else if (req.body.downVote) {
+                beauf.downVotes++;
+            }
+            if (beauf.upVotes > beauf.downVotes) {
+                beauf.isBeauf = true;
+            }
+            else if (beauf.upVotes < beauf.downVotes) {
+                beauf.isBeauf = false;
+            }
+            else {
+                beauf.isBeauf = beauf.originalResult;
+            }
+            beauf.save()
+                .then(() => res.status(200).json({ message: 'Vote enregistré !' }))
+                .catch(error => res.status(400).json({ error }));
+        })
+        .catch(error => res.status(404).json({ error }));
+}
--- models/beauf.js
+++ models/beauf.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const beaufSchema = mongoose.Schema({
     name: { type: String, required: true },
     isBeauf: { type: Boolean, required: true },
+    upVotes: { type: Number, required: true },
+    downVotes: { type: Number, required: true },
+    originalResult: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Beauf', beaufSchema);
